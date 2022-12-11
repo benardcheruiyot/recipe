4e261af71d414477c7a668967c6ac417ffec1fcf
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-12-11 23:52:42 +0300

    modify render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,9 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rails db:drop
+bundle install 
+RAILS_ENV=production rake db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1 
+# bundle exec rails db:drop
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
