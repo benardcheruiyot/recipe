ac365badf19fa80b95e1041b6327d3702d349112
Alex_Wachira_Kimotho <alex.wachira@student.moringaschool.com>
2022-12-11 22:04:32 +0300

    edited the popular recipes text

--- client/src/components/Preview.js
+++ client/src/components/Preview.js
@@ -6,7 +6,7 @@ function Preview() {
       id: 1,
       title: "Chicken salad",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullamsodales, sem a pretium ullamcorper, lorem lectus auctor ligula,semper mattis velit purus vel urna. Duis porta felis ac semposuere convallis.",
+        "This family favorite chicken salad is made with celery, bell pepper, green olives, apple, lettuce, and mayo—plus a secret ingredient that makes all the difference! ",
       imageUrl:
         "https://images.unsplash.com/photo-1574926054530-540288c8e678?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     },
@@ -14,7 +14,7 @@ function Preview() {
       id: 2,
       title: "Roast chicken",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullamsodales, sem a pretium ullamcorper, lorem lectus auctor ligula,semper mattis velit purus vel urna. Duis porta felis ac semposuere convallis.",
+        "The perfect roast chicken doesn’t require a lot of extra ingredients—just some kosher salt, freshly ground pepper, and fresh thyme (and the thyme is optional). Instead, it gets its wonderful flavor and crispy skin from a few simple but essential steps",
       imageUrl:
         "https://images.unsplash.com/photo-1564436872-f6d81182df12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     },
@@ -22,7 +22,7 @@ function Preview() {
       id: 3,
       title: "Fish ",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullamsodales, sem a pretium ullamcorper, lorem lectus auctor ligula,semper mattis velit purus vel urna. Duis porta felis ac semposuere convallis.",
+        "This lemon butter fish recipe is fabulous with any tender, flaky, mild flavored white fish. Any firm white fish such as cod, halibut, snapper, or mahi mahi would work beautifully.",
       imageUrl:
         "https://images.unsplash.com/photo-1611599537845-1c7aca0091c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     },
